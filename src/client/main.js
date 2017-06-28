653f048bb69901b4ac93e7570b8660d52a3b1ad6
Lennart <lennart@lms-dev.com>
2017-06-28 15:00:48 -0400

    Disabled clickable landmarks for the map

--- src/client/main.js
+++ src/client/main.js
@@ -6,6 +6,7 @@ function initMap() {
         zoom: 13,
         minZoom: 13,
         disableDefaultUI: true,
+        clickableIcons: false,
         styles: [{"featureType":"all","elementType":"all","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":-30}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#353535"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#656565"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#505050"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"color":"#808080"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#454545"}]},{"featureType":"transit","elementType":"labels","stylers":[{"hue":"#007bff"},{"saturation":100},{"lightness":-40},{"invert_lightness":true},{"gamma":1.5}]},{"featureType":"transit.station.bus","elementType":"geometry.fill","stylers":[{"color":"#08498f"}]}]
     };
     var map = new google.maps.Map(mapElement, mapOptions);
