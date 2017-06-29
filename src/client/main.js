82bb65b8adef8c7c279c97190632afb110ceef9a
ben <benjenkinsv95@gmail.com>
2017-06-28 20:09:27 -0400

    Reverting tests and refactoring

--- src/client/main.js
+++ src/client/main.js
@@ -1,23 +1,66 @@
-function initMap() {
+var exports = exports || {};
 
+exports.createMapOfBoston = function createMapOfBoston() {
     var mapElement = document.getElementById('map');
     var mapOptions = {
-        center: { lat: 42.3530715, lng: -71.0736387 },
+        center: {lat: 42.3530715, lng: -71.0736387},
         zoom: 13,
         minZoom: 13,
         disableDefaultUI: true,
-        styles: [{"featureType":"all","elementType":"all","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":-30}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#353535"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#656565"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#505050"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"color":"#808080"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#454545"}]},{"featureType":"transit","elementType":"labels","stylers":[{"hue":"#007bff"},{"saturation":100},{"lightness":-40},{"invert_lightness":true},{"gamma":1.5}]},{"featureType":"transit.station.bus","elementType":"geometry.fill","stylers":[{"color":"#08498f"}]}]
+        styles: [{
+            "featureType": "all",
+            "elementType": "all",
+            "stylers": [{"hue": "#ff0000"}, {"saturation": -100},
+                {"lightness": -30}]
+        }, {
+            "featureType": "all",
+            "elementType": "labels.text.fill",
+            "stylers": [{"color": "#ffffff"}]
+        }, {
+            "featureType": "all",
+            "elementType": "labels.text.stroke",
+            "stylers": [{"color": "#353535"}]
+        }, {
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [{"color": "#656565"}]
+        }, {
+            "featureType": "poi",
+            "elementType": "geometry.fill",
+            "stylers": [{"color": "#505050"}]
+        }, {
+            "featureType": "poi",
+            "elementType": "geometry.stroke",
+            "stylers": [{"color": "#808080"}]
+        }, {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{"color": "#454545"}]
+        }, {
+            "featureType": "transit",
+            "elementType": "labels",
+            "stylers": [{"hue": "#007bff"}, {"saturation": 100},
+                {"lightness": -40}, {"invert_lightness": true}, {"gamma": 1.5}]
+        }, {
+            "featureType": "transit.station.bus",
+            "elementType": "geometry.fill",
+            "stylers": [{"color": "#08498f"}]
+        }]
     };
     var map = new google.maps.Map(mapElement, mapOptions);
 
-    google.maps.event.addListener(map, "click", function(event) {
+    google.maps.event.addListener(map, "click", function (event) {
         console.log(map.getCenter().lat(), map.getCenter().lng());
     });
-
-    fetchTruckData(map);
+    return map;
 }
 
-function fetchTruckData(map) {
+function initMap() {
+    var map = exports.createMapOfBoston();
+    fetchTrucks(map);
+}
+
+function fetchTrucks(map) {
     $.ajax({
         url: '/api/trucks',
         type: 'GET',
@@ -39,13 +82,18 @@ function fetchTruckData(map) {
       '</div>'+
       '</div>';
 
-var truck_icon = 'img/truck.png';
+function addMapMarkers(map, data) {
+    data.forEach(function(truck) {
+        addMapMarker(map, truck);
+    });
+}
+
 function addMapMarker(map, truck) {
     var marker = new google.maps.Marker({
         position: {lat: truck.lat, lng: truck.lng},
         map: map,
         title: truck.title,
-        icon: truck_icon
+        icon: 'img/truck.png'
     });
 
     var windowContent = contentString.replace(/__NAME__/g, truck.title);
@@ -58,10 +106,4 @@ function addMapMarker(map, truck) {
     });
 
     return marker;
-}
-
-function addMapMarkers(map, data) {
-    data.forEach(function(truck) {
-        addMapMarker(map, truck);
-    });
 }
